512a4d860b78c3cbb7997dfeab38d65efdf0cfe1
TngocNhut <tranngocnhat2106@gmail.com>
2026-01-08 01:05:11 +0700

    push all code to sub branch nhat

--- src/main/java/vn/hcmute/trainingpoints/entity/notification/NotificationType.java
+++
@@ -1,5 +0,0 @@
-package vn.hcmute.trainingpoints.entity.notification;
-
-public enum NotificationType {
-    SYSTEM, EVENT, REMINDER
-}
--- src/main/java/vn/hcmute/trainingpoints/repository/event/EventCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package vn.hcmute.trainingpoints.repository.event;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vn.hcmute.trainingpoints.entity.event.EventCategory;
-
-public interface EventCategoryRepository extends JpaRepository<EventCategory, Long> {
-}
